3b452cd8da2276fa5a595a5be48f1e7def81721c
Muhammad Akmal <akmalm@Muhammads-MacBook-Pro-2.local>
2021-12-18 15:34:34 +0700

    fix for responsive recipe description index 1 and 2

--- src/views/biteofappetite/DetailRecipe/components/FeaturedArticles/FeaturedArticles.tsx
+++ src/views/biteofappetite/DetailRecipe/components/FeaturedArticles/FeaturedArticles.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Chip from '@mui/material/Chip';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import useWindowDimensions from './useWindowDimensions';
 import SwipeableViews from 'react-swipeable-views';
@@ -20,6 +19,9 @@ const FeaturedArticles = (): JSX.Element => {
     defaultMatches: true,
   });
   const { height } = useWindowDimensions();
+  useEffect(() => {
+    console.log(height);
+  }, [height]);
 
   return (
     <Box>
@@ -38,10 +40,9 @@ const FeaturedArticles = (): JSX.Element => {
               height: 1,
               flexDirection: 'column',
               mr: 2,
-              justifyContent: 'space-evenly',
             }}
           >
-            <RecipeDescription />
+            <RecipeDescription index={0} />
           </Box>
           <Box
             sx={{
@@ -56,137 +57,48 @@ const FeaturedArticles = (): JSX.Element => {
           </Box>
         </Box>
       ) : (
-        <SwipeableViews containerStyle={{ height: height - 58 }} axis="y">
+        <SwipeableViews containerStyle={{ height: height - 90 }} axis="y">
           <Box
             component="div"
             sx={{
               display: 'flex',
               width: 1,
-              height: height - 58,
+              height: height - 90,
               flexDirection: 'column',
-              mr: 1,
-              justifyContent: 'space-evenly',
             }}
           >
-            <RecipeDescription />
+            <RecipeDescription index={0} />
           </Box>
           <Box
             component="div"
             sx={{
               display: 'flex',
               width: 1,
-              height: height - 58,
+              height: height - 90,
               flexDirection: 'column',
-              mr: 1,
-              justifyContent: 'space-evenly',
             }}
           >
-            <Box>
-              <Typography
-                variant={'h3'}
-                fontWeight={700}
-                sx={{
-                  paddingY: 1,
-                }}
-                align={'center'}
-              >
-                Gluten-Free Carrot Cake
-              </Typography>
-              <Typography
-                variant={'subtitle1'}
-                color="text.secondary"
-                fontWeight={500}
-                align={'center'}
-              >
-                Almond flour is a wonderfully sweet, nutty complement for fresh
-                carrots, walnuts, and raisins.
-              </Typography>
-            </Box>
+            <RecipeDescription index={1} />
+          </Box>
+          <Box
+            component="div"
+            sx={{
+              display: 'flex',
+              width: 1,
+              height: height - 90,
+              flexDirection: 'column',
+              mr: 1,
+              justifyContent: 'space-between',
+            }}
+          >
             <Box
               sx={{
                 display: 'flex',
+                height: 1,
                 justifyContent: 'center',
-                alignItems: 'center',
                 flexDirection: 'column',
               }}
             >
-              <Typography
-                variant="overline"
-                color="text.primary"
-                align="center"
-                sx={{
-                  textTransform: 'uppercase',
-                  letterSpacing: 1.5,
-                  fontWeight: 400,
-                  fontSize: 10,
-                  width: 1,
-                }}
-              >
-                Inspired By Joshua Weissman
-              </Typography>
-            </Box>
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'center',
-              }}
-            >
-              <Typography
-                variant="subtitle2"
-                color="text.primary"
-                sx={{
-                  // paddingY: 1,
-                  letterSpacing: 0.4,
-                  // fontWeight: 400,
-                  // fontSize: 14,
-                }}
-              >
-                Total Time: 60 minutes
-              </Typography>
-            </Box>
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                marginY: 0.5,
-                flexWrap: 'wrap',
-                columnGap: 1,
-                rowGap: 1,
-              }}
-            >
-              {mock.map((item) => (
-                <Chip
-                  key={item}
-                  label={item}
-                  component="a"
-                  size={'medium'}
-                  variant={'outlined'}
-                  sx={{
-                    color:
-                      mode === 'light'
-                        ? theme.palette.text.primary
-                        : theme.palette.common.white,
-                    mt: 1,
-                  }}
-                />
-              ))}
-            </Box>
-            <IconButton sx={{ p: 0 }}>
-              <KeyboardArrowDownIcon fontSize="large" />
-            </IconButton>
-          </Box>
-          <Box
-            component="div"
-            sx={{
-              display: 'flex',
-              width: 1,
-              height: height - 58,
-              flexDirection: 'column',
-              mr: 1,
-              justifyContent: 'space-evenly',
-            }}
-          >
-            <Box>
               <Typography
                 variant={'h4'}
                 fontWeight={700}
@@ -207,7 +119,7 @@ const FeaturedArticles = (): JSX.Element => {
                 carrots, walnuts, and raisins.
               </Typography>
             </Box>
-            <IconButton sx={{ p: 0 }}>
+            <IconButton sx={{ pb: 1 }}>
               <KeyboardArrowDownIcon fontSize="large" />
             </IconButton>
           </Box>
@@ -216,7 +128,7 @@ const FeaturedArticles = (): JSX.Element => {
             sx={{
               display: 'flex',
               width: 1,
-              height: height - 58,
+              height: height - 90,
               flexDirection: 'column',
               justifyContent: 'space-evenly',
             }}
--- src/views/biteofappetite/DetailRecipe/components/FeaturedArticles/RecipeDescription.tsx
+++ src/views/biteofappetite/DetailRecipe/components/FeaturedArticles/RecipeDescription.tsx
@@ -11,69 +11,92 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 const mock = ['Easy', 'Indonesian', 'Chicken', 'Dessert'];
 
-const RecipeDescription = (): JSX.Element => {
+interface Props {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  index: number;
+}
+
+const RecipeDescription = ({ index }: Props): JSX.Element => {
   const theme = useTheme();
   const { mode } = theme.palette;
   const isMd = useMediaQuery(theme.breakpoints.up('md'), {
     defaultMatches: true,
   });
 
+  function isMediumOrIndexOne() {
+    return isMd || index == 1;
+  }
+
   return (
-    <Box component="div">
-      <Box
-        sx={{
-          '& .lazy-load-image-loaded': {
-            display: 'flex !important',
-          },
-        }}
-      >
+    <Box
+      component="div"
+      sx={{
+        display: 'flex',
+        height: 1,
+        flexDirection: 'column',
+        rowGap: 1,
+        justifyContent: isMd ? 'center' : 'space-between',
+      }}
+    >
+      {index == 0 && (
         <Box
-          component={LazyLoadImage}
-          height={1}
-          width={1}
-          src={
-            'https://assets.bonappetit.com/photos/61aa54511beaef6a9ff6d6b4/1:1/w_2240,c_limit/20211123%20Jalebi%20LEDE.jpg'
-          }
-          alt="..."
-          effect="blur"
           sx={{
-            objectFit: 'cover',
-            maxHeight: { xs: 320, sm: 370 },
-            borderRadius: 2,
-            justifyContent: 'center',
-            filter: theme.palette.mode === 'dark' ? 'brightness(0.8)' : 'none',
+            '& .lazy-load-image-loaded': {
+              display: 'flex !important',
+            },
           }}
-        />
-        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-          <Typography
-            variant="caption"
-            color="text.primary"
+        >
+          <Box
+            component={LazyLoadImage}
+            height={1}
+            width={1}
+            src={
+              'https://assets.bonappetit.com/photos/61aa54511beaef6a9ff6d6b4/1:1/w_2240,c_limit/20211123%20Jalebi%20LEDE.jpg'
+            }
+            alt="..."
+            effect="blur"
             sx={{
-              letterSpacing: 1.5,
-              fontWeight: 400,
-              fontSize: 10,
-              pt: 1,
+              objectFit: 'cover',
+              maxHeight: { xs: 320, sm: 370 },
+              borderRadius: 2,
+              justifyContent: 'center',
+              filter:
+                theme.palette.mode === 'dark' ? 'brightness(0.8)' : 'none',
             }}
-            align="center"
-          >
-            Food Photography and Food Styling by Muhammad Akmal
-          </Typography>
+          />
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <Typography
+              variant="caption"
+              color="text.primary"
+              sx={{
+                letterSpacing: 1.5,
+                fontWeight: 400,
+                fontSize: { xs: 8, sm: 10 },
+                pt: 1,
+              }}
+              align="center"
+            >
+              Food Photography and Food Styling by Muhammad Akmal
+            </Typography>
+          </Box>
         </Box>
-      </Box>
-      <Box>
+      )}
+      <Box
+        sx={{ display: 'flex', flexDirection: 'column', rowGap: isMd ? 1 : 2 }}
+      >
         <Typography
-          variant={isMd ? 'h4' : 'h3'}
+          variant={isMd ? 'h4' : index == 0 ? 'h3' : 'h1'}
           fontWeight={700}
-          sx={{
-            paddingY: 1,
-          }}
           align={'center'}
+          sx={{
+            mx: isMd ? 0 : 1,
+          }}
         >
           Gluten-Free Carrot Cake
         </Typography>
-        {isMd && (
+        {isMediumOrIndexOne() && (
           <Typography
-            variant={'subtitle1'}
+            variant={isMd ? 'subtitle1' : 'h6'}
             color="text.secondary"
             fontWeight={500}
             align={'center'}
@@ -82,105 +105,123 @@ const RecipeDescription = (): JSX.Element => {
             carrots, walnuts, and raisins.
           </Typography>
         )}
-      </Box>
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: isMd ? 'row' : 'column',
-        }}
-      >
-        <Typography
-          variant="overline"
-          color="text.primary"
-          align="center"
-          sx={{
-            paddingY: isMd ? 1 : 0.5,
-            textTransform: 'uppercase',
-            letterSpacing: 1.5,
-            fontWeight: 400,
-            fontSize: 10,
-            width: isMd ? 1 / 2 : 1,
-          }}
-        >
-          By Muhammad Akmal
-        </Typography>
-        {isMd && (
-          <Divider
-            orientation="vertical"
-            sx={{
-              border: '1px solid',
-              height: '16px',
-            }}
-          />
-        )}
-        {isMd && (
-          <Typography
-            variant="overline"
-            color="text.primary"
-            align="center"
-            sx={{
-              textTransform: 'uppercase',
-              letterSpacing: 1.5,
-              fontWeight: 400,
-              fontSize: 10,
-              width: isMd ? 1 / 2 : 1,
-            }}
-          >
-            Inspired By Joshua Weissman
+        {index == 1 && (
+          <Typography variant="subtitle2" color="text.primary" align={'center'}>
+            Total Time: 60 minutes
           </Typography>
         )}
       </Box>
       <Box
         sx={{
           display: 'flex',
-          justifyContent: 'center',
+          flexDirection: 'column',
+          rowGap: index == 0 ? 1 : 2,
         }}
       >
-        <Typography
-          variant="subtitle2"
-          color="text.primary"
-          sx={{
-            letterSpacing: 0.4,
-          }}
-        >
-          September 20, 2021
-        </Typography>
-      </Box>
-      {!isMd && (
-        <IconButton sx={{ p: 0 }}>
-          <KeyboardArrowDownIcon fontSize="large" />
-        </IconButton>
-      )}
-      {isMd && (
         <Box
           sx={{
             display: 'flex',
             justifyContent: 'center',
-            marginY: 0.5,
-            flexWrap: 'wrap',
-            columnGap: 1,
-            rowGap: 1,
+            alignItems: 'center',
+            flexDirection: isMd ? 'row' : 'column',
           }}
         >
-          {mock.map((item) => (
-            <Chip
-              key={item}
-              label={item}
-              component="a"
-              size={'medium'}
-              variant={'outlined'}
+          {index == 0 && (
+            <Typography
+              variant="overline"
+              color="text.primary"
+              align="center"
               sx={{
-                color:
-                  mode === 'light'
-                    ? theme.palette.text.primary
-                    : theme.palette.common.white,
+                textTransform: 'uppercase',
+                letterSpacing: 1.5,
+                fontWeight: 400,
+                fontSize: 10,
+                width: isMd ? 1 / 2 : 1,
+              }}
+            >
+              By Muhammad Akmal
+            </Typography>
+          )}
+
+          {isMd && (
+            <Divider
+              orientation="vertical"
+              sx={{
+                border: '1px solid',
+                height: '16px',
               }}
             />
-          ))}
+          )}
+          {isMediumOrIndexOne() && (
+            <Typography
+              variant="overline"
+              color="text.primary"
+              align="center"
+              sx={{
+                textTransform: 'uppercase',
+                letterSpacing: 1.5,
+                fontWeight: 400,
+                fontSize: 10,
+                width: isMd ? 1 / 2 : 1,
+              }}
+            >
+              Inspired By Joshua Weissman
+            </Typography>
+          )}
         </Box>
-      )}
+        {index == 0 && (
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            <Typography
+              variant="subtitle2"
+              color="text.primary"
+              sx={{
+                letterSpacing: 0.4,
+              }}
+            >
+              September 20, 2021
+            </Typography>
+          </Box>
+        )}
+
+        {isMediumOrIndexOne() && (
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              // marginY: 0.5,
+              flexWrap: 'wrap',
+              columnGap: 1,
+              rowGap: 1,
+            }}
+          >
+            {mock.map((item) => (
+              <Chip
+                key={item}
+                label={item}
+                component="a"
+                size={'medium'}
+                variant={'outlined'}
+                sx={{
+                  color:
+                    mode === 'light'
+                      ? theme.palette.text.primary
+                      : theme.palette.common.white,
+                }}
+              />
+            ))}
+          </Box>
+        )}
+        {!isMd && (
+          <IconButton sx={{ pb: 1 }}>
+            <KeyboardArrowDownIcon fontSize="large" />
+          </IconButton>
+        )}
+      </Box>
     </Box>
   );
 };
