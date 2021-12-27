5700d92917a8c6a015fc2772af220ea2f7b41015
Muhammad Akmal <akmalm@Muhammads-MacBook-Pro-2.local>
2021-12-27 20:49:48 +0700

    add lets connect button

--- src/blocks/ImageWithDescription/ImageWithDescription.tsx
+++ src/blocks/ImageWithDescription/ImageWithDescription.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Typed from 'react-typed';
+import { Link } from 'react-router-dom';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
@@ -87,37 +88,43 @@ const ImageWithDescription = ({ video }: Props): JSX.Element => {
                   ? 'Food is my way to tell stories and connect with people who share the same passion as I do. Whether its because youre hungry at midnight or trying to learn how to cook, I hope you enjoy watching my content and recipes.'
                   : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'}
               </Typography>
-              <Button
-                variant="outlined"
-                color="primary"
-                sx={{
-                  borderRadius: 30,
-                  border: 2,
-                  borderColor: 'primary.main',
-                  my: 1,
-                  // px: 2,
-                  '&:hover': {
-                    border: 2,
-                  },
+              <Link
+                to="#"
+                onClick={(e) => {
+                  window.location.href = 'mailto:akmalmuhammad51@gmail.com';
+                  e.preventDefault();
                 }}
-                // onClick={() => {
-                //   isRecipe
-                //     ? onClickRecipe(index + (page - 1) * PER_PAGE)
-                //     : onClickWriting(index + (page - 1) * PER_PAGE);
-                // }}
+                style={{ textDecoration: 'none' }}
               >
-                <Typography
-                  variant="button"
-                  color="text.primary"
+                <Button
+                  variant="outlined"
+                  color="primary"
                   sx={{
-                    textTransform: 'uppercase',
-                    letterSpacing: 1.2,
-                    fontWeight: 400,
+                    display: 'flex',
+                    borderRadius: 30,
+                    border: 2,
+                    borderColor: 'primary.main',
+                    my: 1,
+                    // px: 2,
+                    '&:hover': {
+                      border: 2,
+                    },
+                    width: 2 / 3,
                   }}
                 >
-                  Lets make a conversation!
-                </Typography>
-              </Button>
+                  <Typography
+                    variant="button"
+                    color="text.primary"
+                    sx={{
+                      textTransform: 'uppercase',
+                      letterSpacing: 1.2,
+                      fontWeight: 400,
+                    }}
+                  >
+                    Lets make a conversation!
+                  </Typography>
+                </Button>
+              </Link>
             </Box>
             <Box
               maxWidth={{
