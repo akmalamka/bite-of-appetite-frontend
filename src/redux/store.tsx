722d0644d0b3042dc20551f985ba16df281f6911
Muhammad Akmal <akmalm@Muhammads-MacBook-Pro-2.local>
2021-12-27 18:10:14 +0700

    add local storage save redux state

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -2,18 +2,46 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) {
+      return {};
+    }
+    return JSON.parse(serializedState);
+  } catch (e) {
+    return undefined;
+  }
+};
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (e) {
+    // Ignore write errors;
+  }
+};
+
+const persistedState = loadState();
+
 const initialState = {};
 
 const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
-  initialState,
+  // initialState,
+  persistedState,
   compose(
     applyMiddleware(...middleware),
     (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
       (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
   ),
 );
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export default store;
