29623e667d8d6d8b41e0046f20cfb484550b8b6d
akmalamka <akmalmuhammad51@gmail.com>
2022-01-27 10:22:05 +0700

    update video card with dangerouslysetinnerhtml

--- src/blocks/VideoCard/VideoCard.tsx
+++ src/blocks/VideoCard/VideoCard.tsx
@@ -1,37 +1,43 @@
 import React, { useRef, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import { baseUrl } from 'utils/constants';
+import './videoCardClass.css';
+
+const styleObj = {
+  color: 'white',
+  backgroundColor: 'red',
+  objectFit: 'cover',
+};
 
 const VideoCard = (): JSX.Element => {
-  const videoRef = useRef(undefined);
-  useEffect(() => {
-    videoRef.current.defaultMuted = true;
-  });
+  // const videoRef = useRef(undefined);
+  // useEffect(() => {
+  //   videoRef.current.defaultMuted = true;
+  // });
   return (
     <Box display={'flex'} width={{ xs: 1, md: 1 / 2 }}>
-      {/* <div
-        dangerouslySetInnerHTML={{
-          __html: `
-        <Box
-        component={'video'}
+      <Box
+        component="div"
         height={1}
         width={1}
-        autoPlay={true}
-        muted={true}
-        loop={true}
-        sx={{ objectFit: 'cover' }}
-      >
-        <source src={${baseUrl}/pasta-eat.mp4} type="video/mp4" />
-        <source src={${baseUrl}/pasta-eat.webm} type="video/webm" />
-        <source src={${baseUrl}/pasta-eat.ogg} type="video/ogg" />
+        dangerouslySetInnerHTML={{
+          __html: `
+          <video
+          loop
+          muted
+          autoplay
+        />
+        <source src="${baseUrl}/pasta-eat.mp4" type="video/mp4" />
+        <source src="${baseUrl}/pasta-eat.webm" type="video/webm" />
+        <source src="${baseUrl}/pasta-eat.ogg" type="video/ogg" />
         Your browser do not support HTML5 video.
-      </Box>,
+      </video>,
       `,
         }}
-      ></div> */}
-      <Box
+      ></Box>
+
+      {/* <Box
         component={'video'}
-        ref={videoRef}
         height={1}
         width={1}
         autoPlay={true}
@@ -43,7 +49,7 @@ const VideoCard = (): JSX.Element => {
         <source src={`${baseUrl}/pasta-eat.webm`} type="video/webm" />
         <source src={`${baseUrl}/pasta-eat.ogg`} type="video/ogg" />
         Your browser do not support HTML5 video.
-      </Box>
+      </Box> */}
     </Box>
   );
 };
